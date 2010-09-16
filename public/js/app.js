916fe58f41a10777b84af773a0ae0debd02786e2
Dennis Collective <dennis.collective@gmail.com>
2010-09-15 21:55:26 -0700

    mustache

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-    source = "{{#jobs}}<h2>{{position}}</h2><h3>{{company}}</h3><span>{{start-date}} - {{end-date}}</span><ul>  {{#acomplishments}}    <li>{{.}}</li>  {{/acomplishments}}</ul><span>Utilized: {{#tools}}{{.}}, {{/tools}}</span>/jobs}}"
-    template = Handlebars.compile(source)
-    $.getJSON('/json/jobs.json', function(view){
-      html = template(view)
-      $('#main').html(html)
-      });
-    });
+		template = "{{#jobs}}<h2>{{position}}</h2><h3>{{company}}</h3><span>{{start-date}} - {{end-date}}</span><ul>  {{#acomplishments}}    <li>{{.}}</li>  {{/acomplishments}}</ul><span>Utilized: {{#tools}}{{.}}, {{/tools}}</span>{{/jobs}}"
+		$.getJSON('/json/jobs.json', function(view){
+			html = $.mustache(template, view);
+			$('#main').html(html)
+			});
+		});
 
 
