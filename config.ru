d8a48ee6a47b6e8fc6c3ceb406df2d02a9828c72
Dennis Collective <dennis.collective@gmail.com>
2010-09-16 03:22:19 -0700

    adding mustaches to folder, rendering them better

--- config.ru
+++ config.ru
@@ -5,7 +5,7 @@
 #run Resume
 
 #serve static site as everything is now js
-use Rack::Static, :urls => ["/css", "/js", "/json"], :root => "public"
+use Rack::Static, :urls => ["/css", "/js", "/json", "/mustaches"], :root => "public"
 run lambda { |env| [200, { 'Content-Type' => 'text/html', 'Cache-Control' => 'public, max-age=86400' }, File.open('public/index.html', File::RDONLY)] }
 
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,38 @@
-$(document).ready(function(){
-		template = "{{#jobs}}<h2>{{position}}</h2><h3>{{company}}</h3><span>{{start-date}} - {{end-date}}</span><ul>  {{#acomplishments}}    <li>{{.}}</li>  {{/acomplishments}}</ul><span>Utilized: {{#tools}}{{.}}, {{/tools}}</span>{{/jobs}}"
-		$.getJSON('/json/jobs.json', function(view){
-			html = $.mustache(template, view);
-			$('#main').html(html)
-			});
+var templates = {};
+var views = {};
+
+var resume = {
+	fetchTemplate: function(mustache, success){
+	 $.get('/mustaches/'+mustache+'.html', null, function(template){
+			 templates[mustache]=template;
+			 success == null ? null : success()
+			 });
+	},
+
+	fetchView: function(mustache, success) {
+		$.getJSON('/json/'+mustache+'.json', function(view){
+			 views[mustache] = view
+			 success == null ? null : success()
+			 });
+	},								 
+
+	renderMustache: function(mustache, success){ 
+			var html = $.mustache(templates[mustache],views[mustache]);
+			success(html);
+	},
+
+	render:  function(mustache, success){
+		resume.fetchView(mustache, function(){
+			resume.fetchTemplate(mustache, function(){
+				resume.renderMustache(mustache, success);
+			})
 		});
+	}
+};
 
-
+/*$.get("/mustaches/"+A+".html",null,function(D){var C={templates:{}};C.templates[A]=D;twttr.merge(twttr,C,true);B(twttr.templates)},"html")};*/
+$(document).ready(
+	function(){
+		resume.render('jobs', function(html){$('#main').html(html)})
+	}
+);
