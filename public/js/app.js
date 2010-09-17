25154f2015094652d6035077dba2ff21c358b2a0
Dennis Collective <dennis.collective@gmail.com>
2010-09-16 18:44:17 -0700

    better json, stylesheets, cohesion

--- public/js/app.js
+++ public/js/app.js
@@ -3,36 +3,34 @@ var views = {};
 
 var resume = {
 	fetchTemplate: function(mustache, success){
-	 $.get('/mustaches/'+mustache+'.html', null, function(template){
-			 templates[mustache]=template;
-			 success == null ? null : success()
-			 });
+		$.get('/mustaches/'+mustache+'.mustache', null, function(template){
+			templates[mustache]=template;
+			if(success !== null) {success(template);}
+		});
 	},
 
 	fetchView: function(mustache, success) {
 		$.getJSON('/json/'+mustache+'.json', function(view){
-			 views[mustache] = view
-			 success == null ? null : success()
-			 });
+			views[mustache] = view;
+		});
+		if(success !== null) { success(views[mustache]);}
 	},								 
 
 	renderMustache: function(mustache, success){ 
-			var html = $.mustache(templates[mustache],views[mustache]);
-			success(html);
+		var html = $.mustache(templates[mustache],views[mustache]);
+		success(html);
 	},
 
 	render:  function(mustache, success){
 		resume.fetchView(mustache, function(){
 			resume.fetchTemplate(mustache, function(){
 				resume.renderMustache(mustache, success);
-			})
+			});
 		});
 	}
 };
 
-/*$.get("/mustaches/"+A+".html",null,function(D){var C={templates:{}};C.templates[A]=D;twttr.merge(twttr,C,true);B(twttr.templates)},"html")};*/
-$(document).ready(
-	function(){
-		resume.render('jobs', function(html){$('#main').html(html)})
-	}
-);
+$(document).ready(function(){
+	resume.render('jobs', function(html){$('#main').html(html);});
+	resume.render('madagascar', function(html){$('#main').append(html);});
+});
