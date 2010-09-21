6d3e3c90d4bc4eb913646d1bf0fd1150829dd643
Dennis Collective <dennis.collective@gmail.com>
2010-09-21 15:49:57 -0700

    now using extracted glue library

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,4 @@
-var templates = {};
-var views = {};
-
-var resume = {
-	fetchTemplate: function(mustache, success){
-		$.get('/mustaches/'+mustache+'.mustache', null, function(template){
-			templates[mustache]=template;
-			if(success !== null) {success(template);}
-		});
-	},
-
-	fetchView: function(mustache, success) {
-		$.getJSON('/json/'+mustache+'.json', function(view){
-			views[mustache] = view;
-		});
-		if(success !== null) { success(views[mustache]);}
-	},								 
-
-	renderMustache: function(mustache, success){ 
-		var html = $.mustache(templates[mustache],views[mustache]);
-		success(html);
-	},
-
-	render:  function(mustache, success){
-		resume.fetchView(mustache, function(){
-			resume.fetchTemplate(mustache, function(){
-				resume.renderMustache(mustache, success);
-			});
-		});
-	}
-};
-
 $(document).ready(function(){
-	resume.render('jobs', function(html){$('#main').html(html);});
-	resume.render('madagascar', function(html){$('#main').append(html);});
+	glue.render('jobs', '/json/jobs.json', function(html){$('#main').html(html);});
+	glue.render('madagascar', '/json/madagascar.json', function(html){$('#main').append(html);});
 });
