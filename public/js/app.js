58d8324a1f0c2b7e358b617b8ae75f77d65e4b95
Dennis Collective <dennis.collective@gmail.com>
2010-11-05 20:47:31 -0700

    almost using handlebars and such

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,18 @@
 (function($) {
 	var app = $.sammy('#main', function(){
-		this.use(Sammy.Handlebars);
+		this.use(Sammy.Handlebars, 'hb');
+		this.around(function(callback){
+			context = this;
+			this.load('/json/jobs.json')
+			.then(function(jobs){ context.jobs = jobs;})
+			.then(this.load('/json/madagascar.json').then(function(madagascar){context.madagascar = madagascar;}))
+			.then(callback);
+			window.hoe = context;
+		});
+
 		this.get('#/', function(context) {
 			this.load('/json/madagascar.json').then(function(stuff){
-			$.glue.render('jobs', '/json/jobs.json', function(html){$('#main').append(html);});
+				$.glue.render('jobs', '/json/jobs.json', function(html){$('#main').append(html);});
 			}).then(function(){
 				$.glue.render('madagascar', '/json/madagascar.json', function(html){$('#main').append(html);});
 			});
@@ -14,12 +23,5 @@
 		$('#main').html('');
 		app.run('#/');
 	});
+	window.app = app;
 })(jQuery);
-
-/*this.around(function(callback){*/
-/*context = this;*/
-/*this.load('/json/jobs.json')*/
-/*.then(function(jobs){ context.jobs = jobs;})*/
-/**//*.then(this.load('/json/madagascar.json').then(function(madagascar){context.madagascar = madagascar;}))*/
-/*.then(callback);*/
-/*});*/
