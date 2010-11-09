6d57f3628504955f32168d70d0e97fd6cad42ff1
Dennis Collective <dennis.collective@gmail.com>
2010-11-08 17:09:40 -0800

    chrome workiness

--- public/js/app.js
+++ public/js/app.js
@@ -3,25 +3,30 @@
 		this.use(Sammy.Handlebars, 'hb');
 		// this.raise_errors = true;
 		// this.debug = true;
-		this.around(function(callback){
-			var context = this;
+
+		this.helpers({
+			fetchData: function(data){
+				var context = this;
+				this.load('/data/'+data+'.json').then(function(data){ context[data] = data; context.log(context[data]);});
+			}
+		});	
+
+		this.get('#/resume', function(context) {
 			this.load('/data/madagascar.json').then(function(madagascar){ context.madagascar = madagascar;})
 			.then(this.load('/data/jobs.json').then(function(jobs){ context.jobs = jobs;}))
-			.then(callback);
-			window.hoe = context;
-		});
-
-		this.get('#/', function(context) {
-			this.partial('/templates/jobs.hb')
 			.then(function(){
-				this.render('/templates/madagascar.hb').appendTo(context.$element());
+
+				this.partial('/templates/jobs.hb')
+				.then(function(){
+					this.render('/templates/madagascar.hb').appendTo(context.$element());
+				});
 			});
 		});
 	});
 
 	$(document).ready(function(){
 		$('#main').html('');
-		app.run('#/');
+		app.run('#/resume');
 	});
 	window.app = app;
 })(jQuery);
